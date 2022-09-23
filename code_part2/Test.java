d95e985c51e1b6725f4197f9d08c6023ef8d63bc
RecklessSnips <113377805+RecklessSnips@users.noreply.github.com>
2022-09-22 22:13:44 -0400

    Add Test.java file

---
+++ code_part2/Test.java
@@ -0,0 +1,12 @@
+package Assignment_1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Employee Falco = new Employee("Falco", 40, 15.50);
+		System.out.println();
+		System.out.println(Falco.getName());
+		System.out.println(Falco.getHours());
+		System.out.println(Falco.getRate());
+	}
+}
